0fabe8e1d00dcd6e5ba7877fc9979cd40448acd6
declan1397 <declan.gon@gmail.com>
2017-07-21 15:57:00 -0500

    Quickie

--- main.cc
+++ main.cc
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main(int argc, const char * argv[]){
+    cout << "Command line length: " << argc << endl; 
     cout << "Filename: " << argv[1] << endl;
     cout << "Seed: " << argv[2] << endl;
 
