814726acbce6db66edb707f2f1507fb3e7e74a1b
declan1397 <declan.gon@gmail.com>
2017-07-21 16:18:26 -0500

    What is going on

--- main.cc
+++ main.cc
@@ -3,7 +3,44 @@
 using namespace std;
 
 int main(int argc, const char * argv[]){
-    cout << "Command line length: " << argc << endl; 
+    
+    if (argc <= 1){
+        cout << "ERROR: No input file specified.\n";
+    }
+
+    string chosen_word;
+    vector<string> word_bank;
+    ifstream word_file;
+    word_file.open(argv[1]);
+    while (!word_file.eof){
+        string temp;
+        getline(word_file, temp);
+        
+        // Check for >6 characters
+        bool isValid true;
+        if (temp.size() < 6){
+           isValid = false;
+        }
+
+        else { // Check for valid ASCII values
+            for (int i=0; i < temp.size(); i++){
+                if (temp[i] < 'A' || temp[i] > 'z'){
+                    isValid = false;
+                    break;
+                }
+            }
+        }
+
+        if (isValid){
+            word_bank.push_back(temp);
+            cout << "temp\n";
+        }
+
+        isValid = true;
+    }
+    word_file.close();
+
+     
     cout << "Filename: " << argv[1] << endl;
     cout << "Seed: " << argv[2] << endl;
 
