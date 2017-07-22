78edbcf393e0740ddc64c7a6b1ffb82ca19e94ae
declan1397 <declan.gon@gmail.com>
2017-07-21 20:17:00 -0500

    New game implemented

--- main.cc
+++ main.cc
@@ -48,96 +48,114 @@ int main(int argc, const char * argv[]){
     // Play
     /******************/
 
-    mt19937 generator (stoi(argv[2]));
-    chosen_word = word_bank[generator() % word_bank.size()];
-    string guessed_word = "";
-    vector<string> letters_used;
-    int lives_left = 5;
-    string guess;
+    keepPlaying = true;
 
-    // Make guessed word same length as chosen word
-    for (int i=0; i<chosen_word.size(); i++){
-        guessed_word += "-";
-    }
+    while (keepPlaying){
 
-    // Play game loop
-    while (true){
-        cout << "Word: " << guessed_word << "\n";
-        cout << "Letters used: ";
-        for (int i=0; i < letters_used.size(); i++){
-            cout << letters_used[i] << " ";
+        mt19937 generator (stoi(argv[2]));
+        chosen_word = word_bank[generator() % word_bank.size()];
+        string guessed_word = "";
+        vector<string> letters_used;
+        int lives_left = 5;
+        string guess;
+
+        // Make guessed word same length as chosen word
+        for (int i=0; i<chosen_word.size(); i++){
+            guessed_word += "-";
         }
-        cout << "\n";
-        cout << "You have " << lives_left << " lives left.\n";
-        cout << "Next guess: ";
+        
 
-        cin >> guess;
-
-        // If character guess
-        if (guess.size() == 1){
-
-            // If guess is upper case
-            if (guess[0] >= 'A' && guess[0] < 'a'){
-                cout << "Changed " << guess;
-                guess[0] = guess[0] + 32;
-                cout << " to " << guess << endl;
+        // Play game loop
+        while (true){
+            cout << "Word: " << guessed_word << "\n";
+            cout << "Letters used: ";
+            for (int i=0; i < letters_used.size(); i++){
+                cout << letters_used[i] << " ";
             }
-            
-            // If guess is out of range
-            if (guess[0] < 'a' || guess[0] > 'z') {
-                lives_left--;
-                letters_used.push_back(guess);
-                continue;
-            }
+            cout << "\n";
+            cout << "You have " << lives_left << " lives left.\n";
+            cout << "Next guess: ";
 
-            // Guess is valid guess
-            else {
-                cout << "Valid guess" << endl;
-                bool isRepeat = false;
-                // Check if guess has already been made
-                for (int i=0; i < letters_used.size(); i++){
-                    if (letters_used[i] == guess){
-                        isRepeat = true;
-                        break;
-                    }
+            cin >> guess;
+
+            // If character guess
+            if (guess.size() == 1){
+
+                // If guess is upper case
+                if (guess[0] >= 'A' && guess[0] < 'a'){
+                    cout << "Changed " << guess;
+                    guess[0] = guess[0] + 32;
+                    cout << " to " << guess << endl;
+                }
+                
+                // If guess is out of range
+                if (guess[0] < 'a' || guess[0] > 'z') {
+                    lives_left--;
+                    letters_used.push_back(guess);
+                    continue;
                 }
 
-                if (isRepeat){
-                    cout << "You already guessed letter \"" << guess << "\".\n";
-                }
-
+                // Guess is valid guess
                 else {
-                    bool foundIt = false;
-                    cout << "In loop\n";
-                    for (int j=0; j < chosen_word.size(); j++){
-                        if (chosen_word[j] == guess[0] || chosen_word[j] == (guess[0]-32)){
-                            guessed_word[j] = guess[0];
-                            letters_used.push_back(guess);
-                            cout << "Found it!" << endl;
-                            foundIt = true;
+                    cout << "Valid guess" << endl;
+                    bool isRepeat = false;
+                    // Check if guess has already been made
+                    for (int i=0; i < letters_used.size(); i++){
+                        if (letters_used[i] == guess){
+                            isRepeat = true;
+                            break;
                         }
                     }
-                    if (!foundIt){
-                        lives_left--;
-                        letters_used.push_back(guess);
-                    }
-                }
 
+                    if (isRepeat){
+                        cout << "You already guessed letter \"" << guess << "\".\n";
+                    }
+
+                    else {
+                        bool foundIt = false;
+                        cout << "In loop\n";
+                        for (int j=0; j < chosen_word.size(); j++){
+                            if (chosen_word[j] == guess[0] || chosen_word[j] == (guess[0]-32)){
+                                guessed_word[j] = guess[0];
+                                letters_used.push_back(guess);
+                                cout << "Found it!" << endl;
+                                foundIt = true;
+                            }
+                        }
+                        if (!foundIt){
+                            lives_left--;
+                            letters_used.push_back(guess);
+                        }
+                    }
+
+                }
+            }
+
+            // If word guess
+            else if (guess.size() > 1){
+                if (guess == chosen_word){
+                    cout << "You WIN!\n";
+                }
+                else {
+                    cout << "You LOSE!  The word was \"" << chosen_word << "\".\n";
+                }
+                break;
+            }
+
+            // Check if game is won
+            if (boost::iequals(guessed_word, chosen_word)){
+                cout << "You WIN!  The word was \"" << chosen_word << "\".\n";
+                break;
             }
         }
 
-        // If word guess
-        else if (guess.size() > 1){
-
-        }
-
-        // Check if game is won
-        if (boost::iequals(guessed_word, chosen_word)){
-            cout << "You WIN!  The word was \"" << chosen_word << "\".\n";
+        char answer;
+        cout << "Do you want to play again? [Y/N] ";
+        cin >> answer;
+        if (answer != 'y' && answer != 'Y'){
             break;
         }
-
-
+        cout << endl;
     }
 
 
